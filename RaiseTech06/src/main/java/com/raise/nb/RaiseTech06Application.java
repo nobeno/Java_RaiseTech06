b7e6d06d28857800fe085b95cc57439defddba78
daichi nb <nd940501@gmail.com>
2019-10-27 09:57:01 +0900

    first commit

---
+++ RaiseTech06/src/main/java/com/raise/nb/RaiseTech06Application.java
@@ -0,0 +1,13 @@
+package com.raise.nb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiseTech06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiseTech06Application.class, args);
+	}
+
+}
